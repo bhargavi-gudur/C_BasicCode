21eaace0399d6feefb737cf8e0c5db027497f7cd
Gandla Bhargavi <barau.avril@gmail.com>
2024-10-23 22:06:25 +0530

    switch case program

---
+++ switch_case/README.md
@@ -0,0 +1,24 @@
+# Switch Case Arithmetic Operations
+
+This repository contains a C program that demonstrates the use of switch-case statements to perform basic arithmetic operations. The program allows the user to choose an operation to perform on two integers.
+
+## File
+- `switch_case.c`: Program to perform arithmetic operations using switch-case statements.
+
+## Concept
+This project demonstrates the use of switch-case statements in C programming to perform different arithmetic operations based on user input. It covers basic operations like addition, subtraction, modulus, division, and multiplication.
+
+## Build
+To build the project, follow these steps:
+
+1. Ensure you have a C compiler installed on your system, such as `gcc`.
+2. Open your terminal or command prompt.
+3. Navigate to the directory containing the `switch_case.c` file.
+4. Compile the program using the following command:
+   ```sh
+   gcc switch_case.c -o switch_case
+
+   ```sh
+   ./switch_case
+
+   
---
+++ switch_case/switch_case.c
@@ -0,0 +1,65 @@
+/**
+ * @file switch_case.c
+ * @author Gandla Bhargavi
+ * @brief
+ * @version 0.1
+ * @date 2024-10-23
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+#include <stdio.h>
+void switchcase(); // function declartion
+
+/**
+ * @brief main function
+ *
+ * @return int
+ */
+int main()
+{
+    switchcase(); // function calling
+    return 0;
+}
+/**
+ * @brief Function to perform arithmetic operations based on user choice
+ *
+ */
+void switchcase()
+{
+    int choice, a, b;
+    int result;
+    printf("enter two integers a and b\n");
+    scanf("%d%d", &a, &b);
+    printf("enter the choice\n");
+    scanf("%d", &choice);
+    switch (choice)
+    {
+    case 1:
+        result = a + b;
+        printf("addition :%d", result);
+        break;
+    case 2:
+        result = a + b;
+        printf("addition :%d", result);
+        break;
+    case 3:
+        result = a - b;
+        printf("subtraction :%d", result);
+        break;
+    case 4:
+        result = a % b;
+        printf("modulus :%d", result);
+        break;
+    case 5:
+        result = a / b;
+        printf("division :%d", result);
+        break;
+    case 6:
+        result = a * b;
+        printf("mul :%d", result);
+        break;
+    default:
+        printf("invalid value");
+    }
+}
