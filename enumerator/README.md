61aac7ca1e9d8e2f9e74ffe38db331d9324d8809
Gandla Bhargavi <barau.avril@gmail.com>
2024-10-27 09:25:33 +0530

    enumeration and  a switch statement within a function

---
+++ enumerator/README.md
@@ -0,0 +1,14 @@
+# Shape Enumerator
+
+## Description
+This project demonstrates the use of enums in C by enumerating various geometric shapes and displaying them using a loop and a switch statement within a function.
+
+## Files
+- `enumerator.c`: Main source code file containing the implementation of the shapes enumeration and display functions.
+
+## Usage
+1. Clone the repository .
+2. Compile the code using a C compiler, e.g., `gcc enumerator.c -o enumerator`.
+3. Run the executable, e.g., `./enumerator`.
+
+
---
+++ enumerator/enumerator.c
@@ -0,0 +1,80 @@
+/**
+ * @file enumerator.c
+ * @author Gandla Bhargavi
+ * @brief
+ * @version 0.1
+ * @date 2024-10-27
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+#include <stdio.h>
+/**
+ * @brief enumeration of shapes
+ *
+ */
+enum shapes
+{
+    rectangle = 1,
+    triangle,
+    circle,
+    cone,
+    rightangletriangle,
+    sphere,
+    square,
+    elliposidal
+};
+
+void display_shapes(enum shapes EnumShapes); // function declaration
+/**
+ * @brief main function calling the enumeration to loop and display it .
+ *
+ * @return int
+ */
+int main()
+{
+    for (enum shapes EnumShapes = 1; EnumShapes <= elliposidal; EnumShapes++)
+    {
+        display_shapes(EnumShapes);
+    }
+    return 0;
+}
+/**
+ * @brief A function called display_shapes
+ * is declared to take an enum value and print the corresponding shape names.
+ *
+ * @param EnumShapes
+ */
+void display_shapes(enum shapes EnumShapes)
+{
+    switch (EnumShapes)
+    {
+    case rectangle:
+        printf("rectangle -> %d\n", EnumShapes);
+        break;
+    case triangle:
+        printf("triangle-> %d\n", EnumShapes);
+        break;
+    case circle:
+        printf("circle-> %d\n", EnumShapes);
+        break;
+    case cone:
+        printf("cone-> %d\n", EnumShapes);
+        break;
+    case rightangletriangle:
+        printf("rightangletriangle-> %d\n", EnumShapes);
+        break;
+    case sphere:
+        printf("sphere-> %d\n", EnumShapes);
+        break;
+    case square:
+        printf("square-> %d\n", EnumShapes);
+        break;
+    case elliposidal:
+        printf("elliposidal-> %d\n", EnumShapes);
+        break;
+
+    default:
+        printf("invalid command\n");
+    }
+}
