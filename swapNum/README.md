604fa87e6c6c02afbcf2b3a18d1e0f2c159ab808
Gandla Bhargavi <barau.avril@gmail.com>
2024-11-01 23:26:43 +0530

    SWAPNUMBER USING XOR METHOD AND ADDITION/SUBTRACTION METHOD

---
+++ swapNum/README.md
@@ -0,0 +1,33 @@
+# Swap Numbers
+
+This project is a simple C program that demonstrates two different methods to swap the values of two integers without using a temporary variable. It includes traditional addition/subtraction and bitwise XOR operations.
+
+## Features
+
+- Swap two numbers using addition and subtraction.
+- Swap two numbers using bitwise XOR.
+
+## Getting Started
+
+### Prerequisites
+
+- A C compiler (e.g., GCC)
+
+### Installation
+
+1. Clone the repository or download the source code.
+2. Navigate to the project directory.
+
+### Usage
+
+1. Compile the code using the following command:
+    ```sh
+    gcc -o swapNum swapNum.cpp
+    ```
+2. Run the executable:
+    ```sh
+    ./swapNum
+    ```
+
+
+
---
+++ swapNum/swapNum.cpp
@@ -0,0 +1,62 @@
+/**
+ * @file swapNum.cpp
+ * @author Gandla Bhargavi
+ * @brief
+ * -A simple C program that demonstrates two different methods
+ *  to swap the values of two integers without using a temporary variable.
+ * - It includes traditional addition/subtraction and bitwise XOR operations.
+ *
+ * @version 0.1
+ * @date 2024-11-01
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+#include <stdio.h>
+void swapnum();
+void swapnum_Xor();
+/**
+ * @brief  main function
+ *
+ * @return int
+ */
+int main()
+{
+    swapnum();     // function calling
+    swapnum_Xor(); // function calling
+    return 0;
+}
+/**
+ * @brief  this function Swap two numbers using addition and subtraction.
+ *
+ */
+void swapnum()
+{
+    int num1, num2, swapval;
+    printf("enter the num1 and num2 value : ");
+    scanf("%d%d", &num1, &num2);
+    printf("before swap\n");
+    printf("num1[%d],num2[%d]\n", num1, num2);
+    swapval = num1 + num2;
+    num2 = swapval - num2;
+    num1 = swapval - num1;
+    printf("after swap\n");
+    printf("num1[%d],num2[%d],swapval[%d]\n", num1, num2, swapval);
+}
+/**
+ * @brief  this function Swap two numbers using bitwise XOR.
+ *
+ */
+void swapnum_Xor()
+{
+    int num1, num2;
+    printf("enter the num1 and num2 value : ");
+    scanf("%d%d", &num1, &num2);
+    printf("before swap\n");
+    printf("num1[%d],num2[%d]\n", num1, num2);
+    num1 = num1 ^ num2;
+    num2 = num1 ^ num2;
+    num1 = num2 ^ num1;
+    printf("after swap\n");
+    printf("num1[%d],num2[%d]\n", num1, num2);
+}
